c06b16ef9d86007fbc9d399d958ea77c78b8a89b
mike connor <mike@rocketnumber9.org>
2019-01-03 09:55:26 -0600

    App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,74 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
+
+const MyContext = React.createContext();
+
+// provider component
+
+class MyProvider extends Component {
+  state = {
+    name: "mike",
+    age: 100,
+    hair: 'red'
+  }
+  render () {
+    return (
+      <MyContext.Provider value={
+          {
+            state: this.state,
+            growYearOlder: () => {
+              console.log('click');
+              this.setState({
+                age: this.state.age + 1
+              })
+            }
+          }
+        }>
+          {this.props.children}
+      </MyContext.Provider>
+    )
+  }
+}
+
+const Family = (props) => (
+  <div className="family">
+    <Person />
+  </div>
+)
+
+class Person extends Component {
+  render(){
+    return (
+      <div className="person">
+        {/* child of MyContext.Consumer is always a funciton */}
+        <MyContext.Consumer>
+          {(context) => (
+            <React.Fragment>
+              <p>name {context.state.name}</p>
+              <p>hair {context.state.hair}</p>
+              <p>age {context.state.age}</p>
+              <button onClick={context.growYearOlder}>cake</button>
+            </React.Fragment>
+          )}
+        </MyContext.Consumer>
+      </div>
+    )
+  }
+}
 class App extends Component {
+  
   render() {
     return (
+      <MyProvider>
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <Family/>
+         
         </header>
       </div>
+      </MyProvider>
     );
   }
 }
